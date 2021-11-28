665721dd2137df5e716a0884f79ced8844da414c
Deepak Rana <48084363+CodeByRana@users.noreply.github.com>
2021-11-28 13:36:34 +0530

    Update EmployeeWage.java
    Variable value changed here only

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,7 +5,7 @@ public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
 
     public static void main(String[] args) {
 
